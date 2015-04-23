ed20a7694c6e3935bdca7e2640639726d97632d9
33shimori <shimori@pc.jaring.my>
2015-04-23 11:07:21 +0800

    add a simple address list

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,5 @@
 'use strict';
-angular.module('app', []);
+angular.module('app', ['ui.bootstrap']);
 
 
 angular.module('app')
@@ -18,4 +18,25 @@ angular.module('app')
 										"<input ng-model='name.first'></input>" +
 										"<input ng-model='name.last'></input>"
 					}
+});
+
+angular.module('app')
+				.controller('peopleCtrl', function ($scope) {
+					$scope.people = [
+		{name: "John Doe", phone: "3252345678", city: "New York"}, 
+		{name: "Sarah Parker", phone: "123456789", city: "Chicago"},
+		{name: "Little John", phone: "6456135786", city: "Los Angels"},
+		{name: "Adam Doe", phone: "9023548168", city: "Las Vegas"}
+					];
+		$scope.Save=function (){
+			$scope.people.push({
+					name:$scope.newPerson.name,
+					phone:$scope.newPerson.phone, 
+					city:$scope.newPerson.city
+		});
+			$scope.formVisibility = false;
+	};
+		$scope.ShowForm=function (){
+			$scope.formVisibility=true;
+		};			
 });
